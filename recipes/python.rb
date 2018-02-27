6c0263e6090e28cbf4bf88ebc437e2d3a7aa5209
euonymus <euonymus0220@gmail.com>
2018-02-27 17:44:33 +0900

    try

--- recipes/python.rb
+++ recipes/python.rb
@@ -1,4 +1,15 @@
-include_recipe "python"
+python_runtime '3'
+
+python_virtualenv '/var/www/lampapp/.env'
+
+# python_package 'Django' do
+#   version '1.8'
+# end
+
+pip_requirements '/var/www/lampapp/requirements.txt'
+
+
+# include_recipe "python"
 
 # package 'python' do
 #   action :install
