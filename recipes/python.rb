f73e64163dd57797abf9f7140f6b7adf77289fbc
euonymus <euonymus0220@gmail.com>
2018-02-28 15:22:52 +0900

    wsgi

--- recipes/python.rb
+++ recipes/python.rb
@@ -9,7 +9,22 @@ python_virtualenv '/var/www/lampapp/.env'
 pip_requirements '/var/www/lampapp/requirements.txt'
 
 
-# include_recipe "python"
+# include_recipe 'apache2::mod_wsgi'
+case node['platform_family']
+when 'debian'
+  package 'libapache2-mod-wsgi-py3'
+when 'rhel', 'fedora', 'arch', 'amazon'
+  package 'mod_wsgi' do
+    notifies :run, 'execute[generate-module-list]', :immediately
+  end
+end
+
+file "#{node['apache']['dir']}/conf.d/wsgi.conf" do
+  content '# conf is under mods-available/wsgi.conf - apache2 cookbook\n'
+  only_if { ::Dir.exist?("#{node['apache']['dir']}/conf.d") }
+end
+
+apache_module 'wsgi'
 
 # package 'python' do
 #   action :install
